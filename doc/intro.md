d4614a40d0fedfff444af802663db3ecfa37ceb3
Henk Reder <henk@yetanalytics.com>
2020-05-06 08:17:17 -0400

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to vr-transmitter
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/deathtenk/vr_transmitter_test.clj
@@ -0,0 +1,7 @@
+(ns deathtenk.vr-transmitter-test
+  (:require [clojure.test :refer :all]
+            [deathtenk.vr-transmitter :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
